d044b30e8a6c287180f95b224fef4e31d3f1d5fe
Marcos C Lima <marcos.lima.crn@gmail.com>
2018-01-31 13:44:20 -0300

    Estado e cidade

---
+++ src/main/java/com/marcoscarneiro/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.marcoscarneiro.cursomc.repositories;
+
+import com.marcoscarneiro.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
---
+++ src/main/java/com/marcoscarneiro/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.marcoscarneiro.cursomc.repositories;
+
+import com.marcoscarneiro.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
