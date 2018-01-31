d7b9b623f5abb6ed08c5fb440b127a3a5eaa0f12
Marcos C Lima <marcos.lima.crn@gmail.com>
2018-01-31 18:53:49 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/marcoscarneiro/cursomc/domain/Cliente.java
+++ src/main/java/com/marcoscarneiro/cursomc/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.marcoscarneiro.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.marcoscarneiro.cursomc.domain.enums.TipoCliente;
 
@@ -31,6 +32,7 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @JsonBackReference
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
