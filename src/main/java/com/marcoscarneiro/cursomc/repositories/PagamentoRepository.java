79e877b78f37ca071a286a8285db7f5bbc78bab8
Marcos C Lima <marcos.lima.crn@gmail.com>
2018-01-31 16:43:19 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/marcoscarneiro/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.marcoscarneiro.cursomc.repositories;
+
+import com.marcoscarneiro.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
