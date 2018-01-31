dcbc117c6ab798c43876e50978cdce474ec5a7ef
Marcos C Lima <marcos.lima.crn@gmail.com>
2018-01-31 15:08:39 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/marcoscarneiro/cursomc/domain/Cidade.java
+++ src/main/java/com/marcoscarneiro/cursomc/domain/Cidade.java
@@ -1,5 +1,7 @@
 package com.marcoscarneiro.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ public class Cidade implements Serializable {
     private Integer id;
     private String nome;
 
+    @JsonManagedReference
     @ManyToOne
     @JoinColumn(name = "estado_id")
     private Estado estado;
---
+++ src/main/java/com/marcoscarneiro/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.marcoscarneiro.cursomc.services;
+
+import com.marcoscarneiro.cursomc.domain.Cliente;
+import com.marcoscarneiro.cursomc.repositories.ClienteRepository;
+import com.marcoscarneiro.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id){
+        Cliente obj = repo.findOne(id);
+        if(obj == null){
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+        }
+        return obj;
+    }
+}
