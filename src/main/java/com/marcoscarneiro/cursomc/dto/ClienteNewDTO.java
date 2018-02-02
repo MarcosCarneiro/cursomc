9f2da13862a097fa2dac24f069027447fee84c62
Marcos C Lima <marcos.lima.crn@gmail.com>
2018-02-02 16:31:42 -0300

    Notacao de validacao no ClienteNewDTO

--- src/main/java/com/marcoscarneiro/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/marcoscarneiro/cursomc/dto/ClienteNewDTO.java
@@ -1,6 +1,9 @@
 package com.marcoscarneiro.cursomc.dto;
 
 import com.marcoscarneiro.cursomc.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
@@ -9,17 +12,33 @@ public class ClienteNewDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cpfOuCnpj;
+
     private Integer tipo;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String logradouro;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String telefone1;
     private String telefone2;
     private String telefone3;
